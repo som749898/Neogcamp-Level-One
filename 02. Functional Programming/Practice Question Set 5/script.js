d72cdb82cc0701abbef97e70edb3ab78c00be690
Manish S Biswal <manishsom021100@gmail.com>
2023-04-06 13:20:11 +0530

    FP practice set 5

--- 02. Functional Programming/Practice Question Set 5/script.js
+++ 02. Functional Programming/Practice Question Set 5/script.js
@@ -1,1 +1,39 @@
-console.log("Hello");
+// 1. Create an array of objects representing movies in your watchlist. Each object has the following properties: id, title and director. Write a React component that takes the array of movies as input and returns an unordered list of movies, where each list item displays the movie's title and director.
+
+// https://codesandbox.io/s/question-1-1vc9tf
+
+// 2. Create an array of objects representing products in an online store. Each object has the following properties: id, name, price, and category. Write a React component that takes the array of products as input and returns an unordered list of products, where each list item displays the product's name, price, and category.
+
+// https://codesandbox.io/s/question-2-een1hd
+
+// 3. Create an array of objects representing books in a library. Each object has the following properties: id, title, author, and rating. Write a React component that takes the array of books as input and returns an unordered list of books, where each list item displays the book's title, author, and rating.
+
+// https://codesandbox.io/s/question-3-bde5yb
+
+// 4. Create an array of objects representing employees in a company. Each object has the following properties: id, name, department, and salary. Write a React component that takes the array of employees as input and returns an unordered list of employees, where each list item displays the employee's name, department, and salary.
+
+// https://codesandbox.io/s/question-4-8vosc3
+
+// 5. Create an array of objects representing recipes in a cookbook. Each object has the following properties: id, recipe name, recipe creator name. Write a React component that takes the array of recipes as input and returns an unordered list, where each list item displays the recipe's name and recipe creator name.
+
+// https://codesandbox.io/s/question-5-ic755d
+
+// 6. Create an array of objects representing cars in a dealership. Each object has the following properties: id, make, model, and price. Write a React component that takes the array of cars as input and returns an unordered list of cars, where each list item displays the car's make, model, and price.
+
+// https://codesandbox.io/s/question-6-61s6ng
+
+// 7. Create an array of objects representing students in a class. Each object has the following properties: id, name, grade, and attendance. Write a React component that takes the array of students as input and generates an ordered list of students, where each list item displays the student's name, grade, and attendance.
+
+// https://codesandbox.io/s/question-7-2duobq
+
+// 8. Create an array of objects representing movies in your watchlist. Each object has the following properties: id, title, director, and runtime. Write a React component that takes the array of movies as input and generates an ordered list of movies, where each list item displays the movie's title, director, and runtime.
+
+// https://codesandbox.io/s/question-8-z5kp6y
+
+// 9. Create an array of objects representing products in an online store. Each object has the following properties: id, name, price, and category. Write a React component that takes the array of products as input and generates an ordered list of products, where each list item displays the product's name, price, and category.
+
+// https://codesandbox.io/s/question-9-79kdq4
+
+// 10. Create an array of objects representing recipes in a cookbook. Each object has the following properties: id, name, ingredients, and instructions. Write a React component that takes the array of recipes as input and generates an ordered list of recipes, where each list item displays the recipe's name, ingredients, and instructions. Data has been provided below for this questions.
+
+// https://codesandbox.io/s/question-10-30kdki
